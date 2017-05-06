f746b2b7989afce06f8ce82d46b7a7e1b102e08b
Hung Nguyen <hungnguyen@Hungs-MacBook-Air.local>
2017-05-06 09:46:27 +0700

    add func viewwillappear

--- TestGit/StudentViewController.swift
+++ TestGit/StudentViewController.swift
@@ -21,6 +21,9 @@ class StudentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
 
     /*
     // MARK: - Navigation
