a0eb7f9e319541ec887a6934911e6dc2d82855bb
public <PauHV@fsoft.com.vn>
2018-04-09 13:33:13 +0700

    commit a

--- TestGit/StudentViewController.swift
+++ TestGit/StudentViewController.swift
@@ -19,7 +19,7 @@ class StudentViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    } //
     
 
     /*
